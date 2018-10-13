b8f358ebda41c52963e22f615af1ec3ef2fbfc6d
basel-shehabi <44100599+basel-shehabi@users.noreply.github.com>
2018-10-13 21:17:20 +0100

    Add files via upload

---
+++ src/Game.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+import InfoDisplay from './InfoDisplay';
+import UpgradePanel from './UpgradePanel';
+
+class Game extends Component {
+
+    LOOTER_TYPES = [
+        {
+            id: 0,
+            name: "Lame looter",
+            cost: 30,
+            lootPerSecond: 1,
+        },
+        {
+            id: 1,
+            name: "Rookie looter",
+            cost: 100,
+            lootPerSecond: 4,
+        },
+        {
+            id: 2,
+            name: "Intermediate looter",
+            cost: 250,
+            lootPerSecond: 12,
+        },
+        {
+            id: 3,
+            name: "Supreme Boi",
+            cost: 250,
+            strength: 25,
+        },
+    ];
+
+    DEFENDER_TYPES = [
+        {
+            id: 0,
+            name: "Defenseless child",
+            cost: 20,
+            strength: 1,
+        },
+        {
+            id: 1,
+            name: "Jeff",
+            cost: 50,
+            strength: 3,
+        },
+        {
+            id: 2,
+            name: "Sir Killcelot",
+            cost: 100,
+            strength: 8,
+        },
+        {
+            id: 3,
+            name: "Big boi",
+            cost: 250,
+            strength: 25,
+        },
+    ]
+
+    SPECIAL_TYPES = [
+        {
+            id: 1,
+            name: "Can't Touch This",
+            cost: 150,
+            immunity: 1,
+        },
+    ]
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loot: 300,
+            wave: {
+                number: 0,
+                secondsUntil: 60,
+                strength: 5,
+                immunity: 1,
+            },
+            secondsBetweenWaves: 60,
+            lootPerSecond: 1.0,
+            strength: 0.0,
+            immunity: 1.0,
+        };
+
+        this.updateGame = this.updateGame.bind(this);
+        this.upgradeHandler = this.upgradeHandler.bind(this);
+    }
+
+    updateCost(cost) {
+        return Math.round(cost * 1.05);
+    }
+
+    upgradeHandler(e) {
+        // Make sure we don't follow the link
+        e.preventDefault();
+
+        // Extract key information from target
+        const target = e.currentTarget;
+        const type = target.getAttribute('type');
+        const id = target.getAttribute('id');
+
+        // Decide on the type of the upgrade
+        let definition;
+        if (type === 'looter') {
+            definition = this.LOOTER_TYPES[id];
+        } else if (type === 'defender') {
+            definition = this.DEFENDER_TYPES[id];
+        } else if (type === 'special') {
+            definition = this.SPECIAL_TYPES[id];
+        } else {
+            throw new TypeError("You can only upgrade looters, defenders, and specials every now and then.");
+        }
+
+        // Can afford it, so buy it
+        if (this.state.loot >= definition['cost']) {
+            // Subtract loot used for purchase
+            const newState = { ...this.state }
+            newState['loot'] -= definition['cost'];
+            // Update the upgrade cost
+            definition['cost'] = this.updateCost(definition['cost'])
+            // At this point, we've checked it's either looter or defender
+            if (type === 'looter') {
+                newState['lootPerSecond'] += definition['lootPerSecond'];
+            } else if (type === 'defender') {
+                newState['strength'] += definition['strength'];
+            } else {
+                newState['immunity'] += definition['immunity'];
+            }
+            this.setState(newState)
+        } else {
+            alert("No! You can't! Not enough moneyz!")
+        }
+    }
+
+    getNextWaveStrength() {
+        return this.state.wave.strength * 3;
+    }
+
+    isGameOver() {
+        return this.state.strength < 0;
+    }
+
+    updateGame() {
+        // Game over?
+        if (this.state.immunity !== 1) {
+            (this.isGameOver()) 
+            clearInterval(this.gameUpdater);
+            alert("gid gud scrub");
+        }
+
+        // Update the amount of loot we have
+        this.setState({
+            loot: this.state.loot + this.state.lootPerSecond,
+            wave: {
+                ...this.state.wave,
+                secondsUntil: this.state.wave.secondsUntil - 1,
+            },
+        });
+
+        // Wave has arrived
+        if (this.state.wave.secondsUntil <= 0) {
+            // handle wave
+            console.log("Wave has arrived")
+            this.setState({
+                strength: this.state.strength - this.state.wave.strength,
+                immunity: this.state.immunity - 1,
+            })
+
+            // if we survived
+            this.setState({
+                wave: {
+                    ...this.state.wave,
+                    number: this.state.wave.number + 1,
+                    secondsUntil: this.state.secondsBetweenWaves,
+                    strength: this.getNextWaveStrength(),
+                    immunity: this.state.immunity,
+                },
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.gameUpdater = setInterval(this.updateGame, 1000);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <InfoDisplay state={this.state}
+                    looters={this.LOOTER_TYPES}
+                    specials={this.SPECIAL_TYPES}/>
+                <UpgradePanel loot={this.state.loot}
+                    looters={this.LOOTER_TYPES}
+                    defenders={this.DEFENDER_TYPES}
+                    specials={this.SPECIAL_TYPES}
+                    upgradeHandler={this.upgradeHandler} />
+            </div>
+        )
+    }
+
+}
+
+export default Game;
