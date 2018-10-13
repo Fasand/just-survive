6d110e2f870cb2af1fe92550f78caa1ea316eade
Fasand <peter@petermanas.com>
2018-10-13 16:15:13 +0100

    refactored upgrading a bit

--- src/Game.js
+++ src/Game.js
@@ -25,6 +25,27 @@ class Game extends Component {
         },
     ];
 
+    DEFENDER_TYPES = [
+        {
+            id: 0,
+            name: "Defenseless child",
+            cost: 20,
+            strength: 1,
+        },
+        {
+            id: 1,
+            name: "Jeff",
+            cost: 50,
+            strength: 3,
+        },
+        {
+            id: 2,
+            name: "Sir Cancelot",
+            cost: 100,
+            strength: 8,
+        },
+    ]
+
     constructor(props) {
         super(props);
         this.state = {
@@ -49,17 +70,34 @@ class Game extends Component {
         // Make sure we don't follow the link
         e.preventDefault();
 
+        // Extract key information from target
         const target = e.currentTarget;
-        const looter_id = target.getAttribute('looter_id');
-        const looter_def = this.LOOTER_TYPES[looter_id];
+        const type = target.getAttribute('type');
+        const id = target.getAttribute('id');
+        
+        // Decide on the type of the upgrade
+        let definition;
+        if (type === 'looter') {
+            definition = this.LOOTER_TYPES[id];
+        } else if (type === 'defender') {
+            definition = this.DEFENDER_TYPES[id];
+        } else {
+            throw new TypeError("You can only upgrade looters & defenders.");
+        }
         
         // Can afford it, so buy it
-        if (this.state.loot >= looter_def['cost']) {
-            // Subtract loot and add lootPerSecond
-            this.setState({
-                loot: this.state.loot - looter_def['cost'],
-                lootPerSecond: this.state.lootPerSecond + looter_def['lootPerSecond']
-            })
+        if (this.state.loot >= definition['cost']) {
+            // Subtract loot used for purchase
+            const newState = {...this.state}
+            newState['loot'] -= definition['cost'];
+            // At this point, we've checked it's either looter or defender
+            if (type === 'looter') {
+                // Subtract loot and add lootPerSecond
+                newState['lootPerSecond'] += definition['lootPerSecond'];
+            } else {
+                newState['strength'] += definition['strength'];
+            }
+            this.setState(newState)
         } else {
             alert("No! You can't! Not enough moneyz!")
         }
@@ -76,6 +114,7 @@ class Game extends Component {
                                 looters={this.LOOTER_TYPES} />
                 <UpgradePanel loot={this.state.loot}
                             looters={this.LOOTER_TYPES}
+                            defenders={this.DEFENDER_TYPES}
                             upgradeHandler={this.upgradeHandler} />
             </div>
         )
--- src/UpgradePanel.js
+++ src/UpgradePanel.js
@@ -3,23 +3,43 @@ import React from 'react';
 const UpgradePanel = (props) => (
     <div className="row">
         <h2 className="col-12">Upgrade panel</h2>
-        <h3 className="col-12">Looters</h3>
         <div className="col-12">
-        <div className="row">
-            {props.looters.map((looter) => (
-                <a  className="upgradeButton"
-                    href={`#buy-looter-${looter.id}`}
-                    key={looter.id}
-                    looter_id={looter.id} 
-                    onClick={props.upgradeHandler}>
-                    <div>
-                        <p>{looter.name}</p>
-                        <p>{looter.cost}</p>
-                        <p>{looter.lootPerSecond}</p>
-                    </div>
-                </a>
-            ))}
+            <div className="row">
+                <h3 className="col-12">Looters</h3>
+                {props.looters.map((looter) => (
+                    <a  className="upgradeButton"
+                        href={`#buy-looter-${looter.id}`}
+                        key={looter.id}
+                        type="looter"
+                        id={looter.id} 
+                        onClick={props.upgradeHandler}>
+                        <div>
+                            <p>{looter.name}</p>
+                            <p>{looter.cost}</p>
+                            <p>{looter.lootPerSecond}</p>
+                        </div>
+                    </a>
+                ))}
+            </div>
         </div>
+        <div className="col-12">
+            <div className="row">
+                <h3 className="col-12">Defenders</h3>
+                {props.defenders.map((defender) => (
+                    <a  className="upgradeButton"
+                        href={`#buy-defender-${defender.id}`}
+                        key={defender.id}
+                        type="defender"
+                        id={defender.id} 
+                        onClick={props.upgradeHandler}>
+                        <div>
+                            <p>{defender.name}</p>
+                            <p>{defender.cost}</p>
+                            <p>{defender.strength}</p>
+                        </div>
+                    </a>
+                ))}
+            </div>
         </div>
     </div>
 )
