b5365ae804363e58c55edbfd3082556dc41df644
Fasand <peter@petermanas.com>
2018-10-13 12:56:58 +0100

    deleted css, added Game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from './Game';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Game />
     );
   }
 }
---
+++ src/Game.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Hello from the Game</h1>
+            </div>
+        )
+    }
+}
+
+export default Game;
